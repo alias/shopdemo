216838357d15174dede8ba541c197e32f1c70f5b
Bob <bs@oekobox-online.de>
2016-04-11 17:43:06 +0200

    [ant-git-tasks] Git Mirror

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
+/* (C) oekobox-online.eu 2016. Free for use with oekobox-online services */
 /* we use requirejs to load all dependencies */
-
-// start configuration
-var shop_main_pane = 'shop.html?path=';  // must be  relative page. If a page , add ?path=   // todo automatic
-
-
 require(["jquery", "oo"],
     function ($, oo) {
-        console.log("app init man")
+        // code here executed BEFORE framework initialization (if called with ?noinit)
+        console.log("Sample for manual app initialization");
 
         itemMouseOver();  // set up event handlers
 
-        $('BDDY').on('oo-navi-changed', function() {
-                       console.log("on navi")
-                   });
-        /*
-        function appInit() {
-           // oo.ui.addFlash("hola hola!")
-           // oo.ui.addFlashError("hola hola!")
-           // oo.ui.placeFlash();
+        $('#oo-shop').on('oo-shop-changed', function () {
+            console.log("Sample Event Handler")
+        });
 
-        }
+        // alternative way to configure components - in one place
+        /*
+        oo.configureComponents({
+            "shop-page": "shop.html",      // shared from many
+            "cart-page": "cart.html",
+            "logon-page": "logon.html",
+            "profile-page": "profile.html",
+            "date-page": "date.html",
+            "loggedon-page": "shop.html",   // logon component
+            "loggedout-page": "index.html",
+            "register-page": "profile.html"
+        });
         */
 
-       // oo.init(null, null, appInit, false, shopid, shop_main_pane);
+        /* call initWeb() yourself, if  the script was called with ?noinit . shopid is already set for you.
+        function appInit() {
+        // oo.ui.addFlash("hola hola!")              // code executed AFTER framework initialization
+        // oo.ui.addFlashError("hola hola!")
+        // oo.ui.placeFlash();
+
+        }
+        oo.initWeb(appInit, false, shopid);
+        */
 
     }
 );
