b7d726b56ec058012c1ebfaa5c18807162f6cd81
Bob <bs@oekobox-online.de>
2018-08-19 12:19:04 +0200

    [ant-git-tasks] Git Mirror

--- shop.js
+++ shop.js
@@ -1,7 +1,7 @@
 // this little helper script only injects the real path of the script call into the pages. That way, you need
 // to adjust only the following constant to let all pages know about the target system.
 //var SHOPSYSTEM='https://oekobox-online.com/v3/shop/bosshamerschhof2018/api/client.js';
-var SHOPSYSTEM='../..//api/client.js';
+var SHOPSYSTEM='../..//api/client1.js';
 
 // If you build your shop with a server side programming language like PHP, a better approach is a server side include,
 // instead of this file.
