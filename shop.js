ca23b7680dbdf30f8629424af120bab41aac4214
bobsc <bs@oekobox-online.de>
2024-12-28 16:24:42 +0100

    use more recent libs

--- shop.js
+++ shop.js
@@ -1,7 +1,7 @@
 // this little helper script only injects the real path of the script call into the pages. That way, you need
 // to adjust only the following constant to let all pages know about the target system.
 //const SHOPSYSTEM='https://oekobox-online.com/v3/shop/bosshamerschhof2018/api/client.js';
-const SHOPSYSTEM='../../api/client1.js';
+const SHOPSYSTEM='../../api/client2.js';
 
 // If you build your shop with a server side programming language like PHP, a better approach is a server side include,
 // instead of this file.
